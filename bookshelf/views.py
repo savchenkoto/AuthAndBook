a40dd5057cea1da52ddede9b78ac055ac5e1530e
savchenkoto <lapital36@gmail.com>
2017-11-14 01:13:32 +0300

    Start using template shortcut now!

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Author
 
 
 def index(request):
     authors = Author.objects.all()
-    template = loader.get_template('bookshelf/index.html')
-    context = {
-        'authors':authors,
-    }
-    return HttpResponse(template.render(context=context, request=request))
-
+    context = { 'authors':authors }
+    return render(request, 'bookshelf/index.html', context)
 
 def detail(request, author_id):
     return HttpResponse('<h2>Details for Author ' + author_id + '</h2>')
