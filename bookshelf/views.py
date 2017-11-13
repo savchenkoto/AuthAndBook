90ebb1b1b7f098b846047aa37076d7e95b762298
savchenkoto <lapital36@gmail.com>
2017-11-13 23:41:56 +0300

    Second try

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>Welcome!</h1>')
+
+
+def detail(request, book_id):
+    return HttpResponse('<h2>Details for Book ' + book_id + '</h2>')
