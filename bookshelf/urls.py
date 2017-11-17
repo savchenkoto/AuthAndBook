e1ef6b61d4aacfd1fb29652951d4184c1f2fa135
savchenkoto <lapital36@gmail.com>
2017-11-18 02:50:47 +0300

    commit

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,23 +1,24 @@
 from django.conf.urls import url
-from django.views.generic import DetailView
-from .models import *
-from .views import *
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
+from django.urls import reverse_lazy
+from .views import BooksSearch
+from .models import Book, Author
 
 app_name = 'bookshelf'
 
 urlpatterns = [
 
-    url(r'^books/$', Books.as_view(), name='books'),
+    url(r'^books/$', ListView.as_view(model=Book, template_name='bookshelf/books.html'), name='books'),
 
-    url(r'^authors/$', Authors.as_view(), name='authors'),
-
-    url(r'^author/(?P<pk>[0-9]+)/$',
-        DetailView.as_view(model=Author, template_name='bookshelf/author_detail.html'),
-        name='author'),
+    url(r'^authors/$', ListView.as_view(model=Author, template_name='bookshelf/authors.html'), name='authors'),
 
     url(r'^book/(?P<pk>[0-9]+)/$',
         DetailView.as_view(model=Book, template_name='bookshelf/book_detail.html'),
         name='book'),
+
+    url(r'^author/(?P<pk>[0-9]+)/$',
+        DetailView.as_view(model=Author, template_name='bookshelf/author_detail.html'),
+        name='author'),
 
     url(r'author/new/$',
         CreateView.as_view(model=Author, fields=['name', 'about'], template_name='bookshelf/model_form.html'),
@@ -31,16 +32,16 @@ urlpatterns = [
         UpdateView.as_view(model=Author, fields=['name', 'about'], template_name='bookshelf/model_form.html'),
         name='author-update'),
 
-    url(r'book/add/$',
+    url(r'book/new/$',
         CreateView.as_view(model=Book, fields=['author', 'title', 'description'],
                            template_name='bookshelf/model_form.html'),
         name='book-add'),
 
-    url(r'book/(?P<pk>[0-9]+)/delete$',
+    url(r'book/(?P<pk>[0-9]+)/delete/$',
         DeleteView.as_view(model=Book, success_url=reverse_lazy('bookshelf:books')),
         name='book-delete'),
 
-    url(r'book/(?P<pk>[0-9]+)/update$',
+    url(r'book/(?P<pk>[0-9]+)/update/$',
         UpdateView.as_view(model=Book, fields=['author', 'title', 'description'],
                            template_name='bookshelf/model_form.html'),
         name='book-update'),
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -8,26 +8,8 @@ from django.db.models import Q
 from .models import Author, Book
 
 
-class Authors(ListView):
-    template_name = 'bookshelf/authors.html'
-    context_object_name = 'authors'
-    model = Author
-
-    def get_queryset(self):
-        return self.model.objects.all()
-
-
-class Books(ListView):
-    model = Book
-    template_name = 'bookshelf/books.html'
-
-    def get_queryset(self):
-        return self.model.objects.all()
-
-
 class BooksSearch(ListView):
     model = Book
-    context_object_name = 'query_list'
     template_name = 'bookshelf/search_results.html'
 
     def get_queryset(self):
@@ -41,7 +23,5 @@ class BooksSearch(ListView):
         return result
 
     def get_context_data(self, **kwargs):
-        context = super(BooksSearch, self).get_context_data(**kwargs)
-        context.update({'query': self.request.GET.get('q'),
-                        'size': len(self.get_queryset())})
-        return context
+        kwargs['query'] = self.request.GET.get('q')
+        return super().get_context_data(**kwargs)
